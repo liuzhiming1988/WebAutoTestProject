da8c1feb31bc1b8b9f1889023734752efe6fc9f2
qingfeng <554050110@qq.com>
2021-06-13 17:59:11 +0800

    feature:add baidu ocr method

---
+++ tools/ocr_conf.py
@@ -0,0 +1,12 @@
+#!/usr/local/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+@File    : ocr_conf.py
+@Author  : liuzhiming
+@Time    : 2021/6/13 17:18
+"""
+
+APP_ID = "24365769"
+API_KEY = "HxGBCbGNYtmMeQIhzHoQFZmC"
+SECRET_KEY = "dlsmg3BNLnPpBKlUKnheRG6IlBTD2g2m"
