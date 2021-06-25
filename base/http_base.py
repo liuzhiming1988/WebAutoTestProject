ffe3fa5ca22d6e0fd9b64e036828a67052612511
qingfeng <554050110@qq.com>
2021-06-25 19:10:57 +0800

    nothing do

--- base/http_base.py
+++ base/http_base.py
@@ -65,9 +65,11 @@ class HttpBase:
     @headers.setter
     def headers(self, value):
         if isinstance(value, dict):
-            self.__PORT = value
+            self.__HEADERS = value
         else:
             print("Error: 不是dict类型")
+
+
 
     def do_post(self, path, data):
         if self.port is None:
@@ -76,12 +78,21 @@ class HttpBase:
             url = self.protocol + "://" + self.domain + self.port + path
         self.logger.info("Url:{}".format(url))
         self.logger.info("body = {}".format(json.dumps(data)))
-        response = requests.post(url, data=json.dumps(data), headers = self.headers)
+        response = requests.post(url, data=json.dumps(data), headers = self.headers).text
         self.logger.info("{}---response：{}".format(url,response))
         return response
 
     def do_get(self, path):
         requests.get(self.protocol+self.domain+self.port+path)
+
+    @staticmethod
+    def json_format(body):
+        """
+        格式化json字符串，并显示汉字字符，格式缩进更美观
+        :param body:
+        :return:
+        """
+        return json.dumps(body, sort_keys=True, indent=2, ensure_ascii=False)
 
 
 class HttpException:
@@ -90,9 +101,34 @@ class HttpException:
 
 
 if __name__ == '__main__':
-    a = HttpBase()
-    print(a.headers)
-    a.domain = "www.bai.com"
-    print(a.domain)
-    b = HttpBase()
-    print(b.domain)
+    pro_client = HttpBase()
+    path = "/hsbpro"
+    data = {
+  "_head" : {
+    "_remark" : "",
+    "_appVersion" : "5.0.0",
+    "_version" : "0.01",
+    "_groupNo" : "1",
+    "longitude" : 113.94294365618332,
+    "latitude" : 22.532456309056389,
+    "_interface" : "get_merchant_info",
+    "_timestamps" : "1624618031",
+    "_invokeId" : "iOS_C6E0E068-F2F5-4FE2-934C-BD39CB2DF5E2_1624618031",
+    "_msgType" : "request",
+    "channelStr" : "appstore",
+    "_callerServiceId" : "111111"
+  },
+  "_param" : {
+    "queryType" : "tagInfo",
+    "permissions" : "0",
+    "merchantId" : "220395845",
+    "login_token" : "4512751100206992728bcfb5e6d8f458"
+  }
+}
+    pro_client.headers = {
+            "Content-Type":  "application/x-www-form-urlencoded; charset=utf-8"
+        }
+    pro_client.protocol = "https"
+    pro_client.domain = "hsbpro.huishoubao.com"
+    print(pro_client.port)
+    pro_client.do_post(path, data)
